3fe59257871ea74277eac2fc9c661730ed9c3c1f
tommyod <tommy.odland>
2024-11-27 18:22:39 +0100

    typo

--- solvers.py
+++ solvers.py
@@ -556,7 +556,7 @@ def heuristic_search(board: Board, verbose=False, max_nodes=0):
         popped_counter += 1
 
         if popped_counter % max((max_nodes // 100), 1) == 0 and verbose:
-            print(f"Nodes popped: {current.heuristic} Shortest path: {shortest_path}")
+            print(f"Nodes popped: {popped_counter} Shortest path: {shortest_path}")
             print(f"Heuristic function value: {current.heuristic}")
             print(f"Number of moves (depth): {len(current.moves)}")
             print(f"Nodes popped: {popped_counter}")
